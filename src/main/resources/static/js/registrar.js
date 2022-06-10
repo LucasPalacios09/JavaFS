9733dd6b684f08b37b195a5fb3514d5416107d2c
Palacios <Lucas.Palacios@sovos.com>
2022-06-10 11:13:27 -0300

    implementacion agregar usuario

---
+++ src/main/resources/static/js/registrar.js
@@ -0,0 +1,62 @@
+(async function cargarUsuarios(){
+
+    let listadoHtml = '';
+
+    const response = await fetch("api/usuarios",{
+        method: 'GET',
+        headers: {
+            'Accept': 'application/json',
+            'Content-Type': 'application/json'
+        },
+
+    });
+
+        const content = await response.json();
+        //console.log(content);
+
+
+        for (let usuario of content) {
+
+            let usuarioHtml = '<tr><td>' + usuario.id
+            + '</td><td>'+ usuario.nombre
+            +'</td><td>'+ usuario.apellido
+            +'</td><td>'+ usuario.telefono
+            +'</td><td>'+ usuario.email
+            +'</td><td><a href="#" onClick=eliminarUsuario('+usuario.id+') class="btn btn-danger btn-circle btn-sm"><i class="fas fa-trash"></i></a></td></tr>';
+
+            listadoHtml += usuarioHtml;
+        }
+
+            //document.querySelector('#dataTable tbody').outerHTML = listadoHtml;
+
+            const tableHead = '<thead><tr><th>Id</th><th>Nombre</th><th>Apellido</th><th>Telefono</th><th>Email</th><th>Acciones</th></tr></thead>';
+            const tableBody = '<tbody>'+ listadoHtml + '</tbody></table>';
+
+            const table = '<table class="table table-bordered" id="dataTable" width="100%" cellspacing="0">' +
+            tableHead + tableBody +
+            '</table>';
+
+
+            document.getElementById('dataTableDiv').innerHTML = table;
+
+
+    })();
+
+   async function eliminarUsuario(id){
+
+    if (!confirm("¿Seguro que desea eliminar el usuario?")) {
+        return;
+    }
+
+    const response = await fetch("api/usuarios/"+id,{
+            method: 'DELETE',
+            headers: {
+                'Accept': 'application/json',
+                'Content-Type': 'application/json'
+            },
+
+        });
+
+        location.reload();
+
+   }
